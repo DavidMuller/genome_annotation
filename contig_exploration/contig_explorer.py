f37a68af6cdde0571cfbccd6a283cf4fd66ed185
David Muller <davehmuller@gmail.com>
2013-11-14 21:37:48 -0800

    Got rid of commented code in contig_explorer.py

--- contig_exploration/contig_explorer.py
+++ contig_exploration/contig_explorer.py
@@ -4,19 +4,9 @@ from Bio import SeqIO
 sequences = open("scobliq.fasta", "rU")
 for record in SeqIO.parse(sequences, "fasta"):
     print "\n"    
-    print record.id    
+    print record.id   
     print len(record.seq)
 sequences.close()
 
 
-"""
-# print contigs longer than 125000...
-sequences = open("scobliq.fasta", "rU")
-total = 0
-for record in SeqIO.parse(sequences, "fasta"):
-    if len(record.seq) > 125000:
-        print "\n"    
-        print record.id    
-        print len(record.seq)
-sequences.close()
-"""
+
